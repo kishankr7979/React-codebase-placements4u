de061d6837749b980d1bbe799d2ecf09b0a7be77
Kishan Kumar <41482800+kishankr7979@users.noreply.github.com>
2021-10-21 00:57:12 +0530

    openings update

--- src/ComList/latestlist.js
+++ src/ComList/latestlist.js
@@ -1,34 +1,34 @@
 export const LatestOpenings = [
     {
         "id": "1",
-        "Company": "Amazon | SDE 1 | Freshers | 32 LPA",
-        "Careers": "https://www.amazon.jobs/en-gb/jobs/1584041/sde-i"
+        "Company": "Customized Software | BE/BTech | 2020, 2021 batch",
+        "Careers": "https://www.elitmus.com/jobs/32768-customised-technologies-software-engineer-bangalore?jobs_evaluation=1963"
     },
     {
         "id": "2",
-        "Company": "Microchip | Freshers",
-        "Careers": "https://careers.microchip.com/job/3356/engineer_software"
+        "Company": "ON Semiconductor | Freshers",
+        "Careers": "https://onsemi.taleo.net/careersection/on_ex/jobdetail.ftl?job=2100986&iniurl.src=JB-10142&tz=GMT-05%3A00&tzname=America%2FChicago"
 
     },
     {
         "id": "3",
-        "Company": "Orange Business Services | Freshers",
-        "Careers": "https://orange.jobs/jobs/similar.do?joid=98554&paramuid=C9E45068-F189-403D-AF96-7F3D78270F9B&wmode=all&lang=EN"
+        "Company": "24*7 ai | Freshers",
+        "Careers": "https://www.myamcat.com/jobs/description/software-development-engineer-job-for-fresher-in-24-7ai-in-bengaluru/89247/?source=offcampus"
     },
     {
         "id": "4",
-        "Company": "Altran | Fresher ",
-        "Careers": "https://career10.successfactors.com/career?career_ns=job_listing&company=C0002723615P&navBarLevel=JOB_SEARCH&career_job_req_id=76136"
+        "Company": "Virtusa Corp | Referral Drive | Fresher ",
+        "Careers": "https://www.monsterindia.com/seeker/registration?spl=IN-Acq-Display_Direct-Adsolave_OCJ4U-IT/BPO_Exp-Dec_20-Multiple-Banners/Text-Web+WAP-CPC&utm_campaign=IN_Acq_Display_Direct_IT/BPO_Exp_Multiple_Banners/Text&utm_medium=Display_Direct&utm_source=Adsolave_OCJ4U-Dec_20-CPC&utm_content=Display_Direct_Banners/Text"
     },
     {
         "id": "5",
-        "Company": "Rubrik Inc Hiring For Software Engineer Position",
-        "Careers": "https://boards.greenhouse.io/rubrik/jobs/2280345"
+        "Company": "Metrcstream | MTS | Freshers",
+        "Careers": "https://match.myanatomy.in/corporate/customCampaign/view?publicLink=ebc1591f565ce9ac0bdef64f19b98228;2fc18d9709cf0156f3b50278b5259800bae6b7ba869612be0bebc5e25f2f262c4ad6cdb519ca8dcd2b214f409d96d6075b15241374628903c049941a3f40a7066cac42338155b96c669b5f57c5667186a2f74be40ac710494bc7485e82c2495aaebd07da069205c6def02e47de8c41116ed83883a7e105d3784ad6e9444bea7b9cb57d4c2427ed745a8b9e4eebc26c5a260034253cadc036504807d7b3234788167f033e7c34bc7f95d5aa0d328163705b8a16c6442b7b8344d6307742edc5b8&source=n"
     },
     {
         "id": "6",
-        "Company": "Medtronic | Freshers",
-        "Careers": "https://jobs.medtronic.com/jobs/software-engineer-i-101840"
+        "Company": "Nutanix | Freshers",
+        "Careers": "https://nutanix.eightfold.ai/careers?pid=9773265&domain=nutanix.com"
     },
 
 
